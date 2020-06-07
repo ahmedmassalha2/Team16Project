a92f02053474a88b3de52e1d3c384edbb45b9653
Ahmed Massalha <ahmedmassalha2@gmail.com>
2020-06-07 15:09:32 +0300

    hibernate changes

--- project/src/main/java/com/example/entities/checkedExam.java
+++ project/src/main/java/com/example/entities/checkedExam.java
@@ -2,43 +2,108 @@ package com.example.entities;
 
 import java.util.List;
 
-import net.bytebuddy.asm.Advice.This;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 
-public class checkedExam extends Exam {
+@Entity
+public class checkedExam {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name = "teacher_id")
+	private Teacher teacher;
+	
+	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name = "subject_id")
+	private Subject subject;
+	
+	@ManyToMany
+	private List<Question> questions;
+	private String timeString;
+	
+	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name = "student_id")
 	private Student student;
+	
 	private double grade;
 	private String discreption;
-
+	
+	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name = "course_id")
+	private Course course;
 	public checkedExam(Teacher teacher, Subject subject, List<Question> questions, String timeString, Student student,
 			double grade, String discreption) {
-		super(teacher, subject, questions, timeString);
+		this.teacher = teacher;
+		this.subject = subject;
+		this.questions = questions;
+		this.timeString = timeString;
 		this.student = student;
 		this.grade = grade;
 		this.discreption = discreption;
 	}
+	public checkedExam() {
 
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Teacher getTeacher() {
+		return teacher;
+	}
+	public void setTeacher(Teacher teacher) {
+		this.teacher = teacher;
+	}
+	public Subject getSubject() {
+		return subject;
+	}
+	public void setSubject(Subject subject) {
+		this.subject = subject;
+	}
+	public List<Question> getQuestions() {
+		return questions;
+	}
+	public void setQuestions(List<Question> questions) {
+		this.questions = questions;
+	}
+	public String getTimeString() {
+		return timeString;
+	}
+	public void setTimeString(String timeString) {
+		this.timeString = timeString;
+	}
 	public Student getStudent() {
 		return student;
 	}
-
 	public void setStudent(Student student) {
 		this.student = student;
 	}
-
 	public double getGrade() {
 		return grade;
 	}
-
 	public void setGrade(double grade) {
 		this.grade = grade;
 	}
-
 	public String getDiscreption() {
 		return discreption;
 	}
-
 	public void setDiscreption(String discreption) {
 		this.discreption = discreption;
 	}
+	
+	
+
+
 
 }
